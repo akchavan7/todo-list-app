293b64807ef7fb67d4ba85f000bdd87305844fb6
Akshay Chavan <akchavan@MB-C02DFDWFMD6M.local>
2024-07-17 11:18:19 -0500

    create backend service and routes for DB operations

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -3,8 +3,43 @@ var mysql = require("mysql");
 const router = express.Router();
 const todoOps = require("../services/todoOps");
 
-router.get("/test", async (req, res) => {
-  res.json(await todoOps.getCurrentTasks());
+router.get("/getCurrentTasks", async (req, res, next) => {
+  try {
+    res.json(await todoOps.getCurrentTasks());
+  } catch (err) {
+    console.log(`Error while fetching current tasks`);
+    next(err);
+  }
+});
+
+router.get("/getArchivedTasks", async (req, res, next) => {
+  try {
+    res.json(await todoOps.getArchivedTasks());
+  } catch (err) {
+    console.log(`Error while fetching archived tasks`);
+    next(err);
+  }
+});
+
+router.post("/addTask", async (req, res, next) => {
+  try {
+    const text = req.body.text;
+    res.json(await todoOps.addTask(text));
+  } catch (err) {
+    console.log(`Error while trying to add a new task`);
+    next(err);
+  }
+});
+
+router.post("/updateTask", async (req, res, next) => {
+  try {
+    const id = req.body.id;
+    const updatedText = req.body.updatedText;
+    res.json(await todoOps.updateTask(id, updatedText));
+  } catch (err) {
+    console.log(`Error while trying to update the task`);
+    next(err);
+  }
 });
 
 module.exports = router;
--- server/services/todoOps.js
+++ server/services/todoOps.js
@@ -1,8 +1,33 @@
 const execute = require("../helpers/queryRunner");
 
 async function getCurrentTasks() {
-  const rows = await execute(`SELECT * FROM todo_list`);
+  const rows = await execute(`SELECT * FROM todo_list WHERE status="Pending"`);
   return rows;
 }
 
-module.exports = { getCurrentTasks };
+async function getArchivedTasks() {
+  const rows = await execute(`SELECT * FROM todo_list WHERE status="Done"`);
+  return rows;
+}
+
+async function addTask(text) {
+  const result = await execute(
+    `INSERT INTO todo_list(text) values("${text}");`
+  );
+  if (result.affectedRows === 1) {
+    return { status: 200, message: "New task added successfully" };
+  }
+  return { status: 401, message: "Could not add new task" };
+}
+
+async function updateTask(id, updatedText) {
+  const result = await execute(
+    `UPDATE todo_list SET text = "${updatedText}" WHERE id = ${id};`
+  );
+  if (result.affectedRows === 1) {
+    return { status: 200, message: "Task has been updated successfully" };
+  }
+  return { status: 401, message: "Could not update the task" };
+}
+
+module.exports = { getCurrentTasks, getArchivedTasks, addTask, updateTask };
